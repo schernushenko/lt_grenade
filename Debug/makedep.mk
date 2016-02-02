b0b5e34a416e761ee22415758111e745195ff7d4
unknown <csv@spbnb-prc187.t-systems.ru>
2016-02-03 01:31:33 +0300

    new pin for ir reciever

---
+++ Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Grenade.cpp
+
+IRMiles.cpp
+
+IRMilesK.cpp
+
+miles.cpp
+
